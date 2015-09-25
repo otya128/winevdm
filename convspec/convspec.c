80c00b84e5aee0c36500c0928051ebd6a2de638a
otya128 <otya128@users.noreply.github.com>
2015-09-25 20:36:28 +0900

    shell.dllを追加,convspecにｰDEFオプションを追加

--- convspec/convspec.c
+++ convspec/convspec.c
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
 {
 	if (argc <= 1)
 	{
-		printf("wine spec file convert tool\nusage: %s specfile(16bit only) module name\n", argv[0]);
+		printf("wine spec file convert tool\nusage: %s specfile(16bit only) module name\n%s specfile(16bit only) -DEF\n", argv[0], argv[0]);
 		fatal_error("file argument '%s' not allowed in this mode\n", argv[0]);
 	}
 	DLLSPEC *spec = alloc_dll_spec();
@@ -21,8 +21,15 @@ int main(int argc, char* argv[])
 	//	init_dll_name(spec);
 	if (argc > 2)
 	{
-		spec->main_module = xstrdup(argv[2]);
-		spec->dll_name = xstrdup(argv[2]);
+		if (!strcmp(argv[2], "-DEF"))
+		{
+			exec_mode = MODE_DEF;
+		}
+		else
+		{
+			spec->main_module = xstrdup(argv[2]);
+			spec->dll_name = xstrdup(argv[2]);
+		}
 	}
 	else
 	{
@@ -30,6 +37,11 @@ int main(int argc, char* argv[])
 	}
 	switch (exec_mode)
 	{
+	case MODE_DEF:
+		if (!spec_file_name) fatal_error("missing .spec file\n");
+		if (!parse_input_file(spec)) break;
+		output_def_file(spec, 1);
+		break;
 	case MODE_DLL:
 		if (spec->subsystem != IMAGE_SUBSYSTEM_NATIVE)
 			spec->characteristics |= IMAGE_FILE_DLL;
