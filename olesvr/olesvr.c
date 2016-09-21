9bd514970be98272db6457957a0571fdf5fe5b1c
otya <otya128@users.noreply.github.com>
2016-09-21 18:18:28 +0900

    OLESVR.DLLを移植

--- olesvr/olesvr.c
+++ olesvr/olesvr.c
@@ -224,16 +224,3 @@ OLESTATUS WINAPI OleSavedServerDoc16(LHSERVERDOC hDoc)
 	FIXME("(%d): stub.\n", hDoc);
 	return OLE_OK;
 }
-//
-void __wine_spec_init_ctor()
-{
-	DPRINTF("NOTIMPL:__wine_spec_init_ctor()\n");
-}
-void __wine_spec_unimplemented_stub(const char *module, const char *function)
-{
-	DPRINTF("NOTIMPL:__wine_spec_unimplemented_stub(%s, %s)\n", module, function);
-}
-void __wine_spec_dll_entry()
-{
-	DPRINTF("NOTIMPL:__wine_spec_dll_entry(?)\n");
-}
