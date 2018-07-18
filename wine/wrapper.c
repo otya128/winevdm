078333f0ca367655849530023ae78f3c1de33749
otya <otya128@users.noreply.github.com>
2018-07-19 01:33:25 +0900

    LoadLibraryAWrappesa

--- wine/wrapper.c
+++ wine/wrapper.c
@@ -1,6 +1,20 @@
 #include <Windows.h>
+#include <Shlwapi.h>
 //PROGMAN.EXEÇ»Ç«Ç≈COMMDLGÇ™ì«Ç›çûÇ‹ÇÍÇ»Ç©Ç¡ÇΩÇÃÇ≈èCê≥
 HMODULE LoadLibraryAWrapper(const char *name)
 {
-    return LoadLibraryA(name);
+    HMODULE m = LoadLibraryA(name);
+    if (m)
+        return m;
+    else
+    {
+        char buf[MAX_PATH];
+        char buf2[MAX_PATH];
+        GetModuleFileNameA(GetModuleHandleA("libwine.dll"), buf, sizeof(buf));
+        *PathFindFileNameA(buf) = '\0';
+        PathCombineA(buf2, buf, name);
+        
+        m = LoadLibraryA(buf2);
+    }
+    return m;
 }
