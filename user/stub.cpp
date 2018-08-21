336001e8785dd571ecc7a166650a4edd013a64f2
otya <otya128@users.noreply.github.com>
2018-08-21 13:36:27 +0900

    Remove stub.c

--- user/stub.cpp
+++ user/stub.cpp
@@ -47,18 +47,6 @@ int ab()
 }
 extern "C"
 {
-	void __wine_spec_init_ctor()
-	{
-		DPRINTF("NOTIMPL:__wine_spec_init_ctor()\n");
-	}
-	void __wine_spec_unimplemented_stub(const char *module, const char *function)
-	{
-		DPRINTF("NOTIMPL:__wine_spec_unimplemented_stub(%s, %s)\n", module, function);
-	}
-	void __wine_spec_dll_entry()
-	{
-		DPRINTF("NOTIMPL:__wine_spec_dll_entry(?)\n");
-	}
 	/***********************************************************************
 	*		SignalProc32 (USER.391)
 	*		UserSignalProc (USER32.@)
