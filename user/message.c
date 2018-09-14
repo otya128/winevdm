e6c2c0b7a68fa89dc163d9878699c58f1ac8a517
otya <otya128@users.noreply.github.com>
2018-09-14 21:03:29 +0900

    Fix WM_DDE_EXECUTE conversion

--- user/message.c
+++ user/message.c
@@ -1427,7 +1427,7 @@ LRESULT WINPROC_CallProc16To32A( winproc_callback_t callback, HWND16 hwnd, UINT1
         break; /* FIXME don't know how to free allocated memory (handle) !! */
     case WM_DDE_EXECUTE:
         lParam = convert_handle_16_to_32( HIWORD(lParam), GMEM_DDESHARE );
-        ret = callback( hwnd32, msg, wParam, lParam, result, arg );
+        ret = callback( hwnd32, msg, HWND_32(wParam), lParam, result, arg );
         break; /* FIXME don't know how to free allocated memory (handle) !! */
     case WM_PAINTCLIPBOARD:
     case WM_SIZECLIPBOARD:
@@ -1928,7 +1928,7 @@ LRESULT WINPROC_CallProc32ATo16( winproc_callback16_t callback, HWND hwnd, UINT 
         break; /* FIXME don't know how to free allocated memory (handle) !! */
     case WM_DDE_EXECUTE:
         lParam = MAKELPARAM( 0, convert_handle_32_to_16( lParam, GMEM_DDESHARE ));
-        ret = callback( HWND_16(hwnd), msg, wParam, lParam, result, arg );
+        ret = callback( HWND_16(hwnd), msg, HWND_16(wParam), lParam, result, arg );
         break; /* FIXME don't know how to free allocated memory (handle) !! */
     case SBM_SETRANGE:
         ret = callback( HWND_16(hwnd), SBM_SETRANGE16, 0, MAKELPARAM(wParam, lParam), result, arg );
