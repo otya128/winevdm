2752a974b06accba10ba41dc8bdd7517891e0817
otya <otya128@users.noreply.github.com>
2018-10-07 20:04:27 +0900

    Fix LCMapStringA

--- ole2nls/ole2nls.c
+++ ole2nls/ole2nls.c
@@ -40,6 +40,8 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(ole);
 
+/* FIXME: other language flags? */
+
 /* same as win32 */
 #define NORM_IGNORECASE16           0x00000001
 #define NORM_IGNORENONSPACE16       0x00000002
@@ -54,6 +56,17 @@ WINE_DEFAULT_DEBUG_CHANNEL(ole);
 
 #define NORM_ALL16 (NORM_IGNORECASE16 | NORM_IGNORENONSPACE16 | NORM_IGNORESYMBOLS16 | NORM_IGNOREWIDTH16 | NORM_IGNOREKANATYPE16 | NORM_IGNOREJAPANACCENT16 | NORM_IGNOREKASHIDA16)
 
+/* same as win32 */
+#define LCMAP_LOWERCASE16    0x00000100
+#define LCMAP_UPPERCASE16    0x00000200
+#define LCMAP_SORTKEY16      0x00000400
+/* different from win32 */
+#define LCMAP_HALFWIDTH16    0x00000800
+#define LCMAP_FULLWIDTH16    0x00001000
+#define LCMAP_HIRAGANA16     0x00002000
+#define LCMAP_KATAKANA16     0x00004000
+
+#define LCMAP_ALL16 (LCMAP_LOWERCASE16 | LCMAP_UPPERCASE16 | LCMAP_SORTKEY16 | LCMAP_HALFWIDTH16 | LCMAP_FULLWIDTH16 | LCMAP_HIRAGANA16 | LCMAP_KATAKANA16)
 static LPVOID lpNLSInfo = NULL;
 
 /******************************************************************************
@@ -106,25 +119,9 @@ LANGID WINAPI GetSystemDefaultLangID16(void)
     return GetSystemDefaultLangID();
 }
 
-/******************************************************************************
- *		LCMapStringA	[OLE2NLS.6]
- */
-INT16 WINAPI LCMapString16(LCID lcid, DWORD mapflags, LPCSTR srcstr, INT16 srclen,
-			   LPSTR dststr, INT16 dstlen)
-{
-    return LCMapStringA(lcid, mapflags, srcstr, srclen, dststr, dstlen);
-}
-
-/***********************************************************************
- *           CompareStringA       (OLE2NLS.8)
- */
-INT16 WINAPI CompareString16(LCID lcid, DWORD flags16, LPCSTR str1, INT16 len1, LPCSTR str2, INT16 len2)
+static DWORD convert_NORM16_to_NORM(DWORD flags16)
 {
     DWORD flags = 0;
-    if ((flags16 & ~NORM_ALL16) != 0)
-    {
-        return 0;
-    }
     if (flags16 & NORM_IGNORECASE16)
         flags |= NORM_IGNORECASE;
     if (flags16 & NORM_IGNORENONSPACE16)
@@ -145,6 +142,54 @@ INT16 WINAPI CompareString16(LCID lcid, DWORD flags16, LPCSTR str1, INT16 len1, 
         FIXME("NORM_IGNOREKASHIDA\n");
         flags |= 0;
     }
+    return flags;
+}
+static DWORD convert_LCMAP16_to_LCMAP(DWORD mapflags16)
+{
+    DWORD mapflags = 0;
+    if (mapflags16 & LCMAP_LOWERCASE16)
+        mapflags |= LCMAP_LOWERCASE;
+    if (mapflags16 & LCMAP_UPPERCASE16)
+        mapflags |= LCMAP_UPPERCASE;
+    if (mapflags16 & LCMAP_SORTKEY16)
+        mapflags |= LCMAP_SORTKEY;
+    if (mapflags16 & LCMAP_HALFWIDTH16)
+        mapflags |= LCMAP_HALFWIDTH;
+    if (mapflags16 & LCMAP_FULLWIDTH16)
+        mapflags |= LCMAP_FULLWIDTH;
+    if (mapflags16 & LCMAP_HIRAGANA16)
+        mapflags |= LCMAP_HIRAGANA;
+    if (mapflags16 & LCMAP_KATAKANA16)
+        mapflags |= LCMAP_KATAKANA;
+    return mapflags | convert_NORM16_to_NORM(mapflags16);
+}
+
+/******************************************************************************
+ *		LCMapStringA	[OLE2NLS.6]
+ */
+INT16 WINAPI LCMapString16(LCID lcid, DWORD mapflags16, LPCSTR srcstr, INT16 srclen,
+			   LPSTR dststr, INT16 dstlen)
+{
+    DWORD mapflags = 0;
+    if ((mapflags16 & ~(LCMAP_ALL16 | NORM_ALL16)) != 0)
+    {
+        return 0;
+    }
+    mapflags = convert_LCMAP16_to_LCMAP(mapflags16);
+    return (INT16)LCMapStringA(lcid, mapflags, srcstr, srclen, dststr, dstlen);
+}
+
+/***********************************************************************
+ *           CompareStringA       (OLE2NLS.8)
+ */
+INT16 WINAPI CompareString16(LCID lcid, DWORD flags16, LPCSTR str1, INT16 len1, LPCSTR str2, INT16 len2)
+{
+    DWORD flags = 0;
+    if ((flags16 & ~NORM_ALL16) != 0)
+    {
+        return 0;
+    }
+    flags = convert_NORM16_to_NORM(flags16);
     return CompareStringA(lcid, flags, str1, len1, str2, len2);
 }
 
