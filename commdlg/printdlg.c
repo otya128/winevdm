81523f14b88e02651b5a90f08cf242531e65df8b
otya <otya128@users.noreply.github.com>
2018-10-21 10:23:42 +0900

    Add support for PD_ENABLEPRINTTEMPLATE, PD_ENABLESETUPTEMPLATE

--- commdlg/printdlg.c
+++ commdlg/printdlg.c
@@ -80,6 +80,19 @@ static HGLOBAL global_handle_from_16( HGLOBAL16 handle )
     return ret;
 }
 
+DLGTEMPLATE *WINAPI dialog_template16_to_template32(HINSTANCE16 hInst, LPCVOID dlgTemplate, DWORD *size);
+LPDLGTEMPLATEA *resource_to_dialog32(HINSTANCE16 hInst, LPCSTR name)
+{
+    HRSRC16 res = FindResource16(hInst, name, (LPCSTR)RT_DIALOG);
+    HGLOBAL16 handle = LoadResource16(hInst, res);
+    DWORD size = SizeofResource16(hInst, res);
+    void *ptr = LockResource16(handle);
+    DWORD size2;
+
+    LPDLGTEMPLATEA *r = dialog_template16_to_template32(hInst, ptr, &size2);
+    FreeResource16(handle);
+    return r;
+}
 /**********************************************************************
  *
  *      16 bit commdlg
@@ -105,6 +118,9 @@ BOOL16 WINAPI PrintDlg16( SEGPTR pd )
     LPPRINTDLG16 lppd = MapSL(pd);
     PRINTDLGA pd32;
     BOOL ret;
+    HINSTANCE16 hInst;
+    LPDLGTEMPLATEA template_setup = NULL;
+    LPDLGTEMPLATEA template_print = NULL;
 
     if (!lppd) return PrintDlgA(NULL); /* generate failure with CDERR_INITIALIZATION */
 
@@ -122,10 +138,23 @@ BOOL16 WINAPI PrintDlg16( SEGPTR pd )
     pd32.nCopies     = lppd->nCopies;
     pd32.lpfnPrintHook = NULL;
     pd32.lpfnSetupHook = NULL;
+    hInst = lppd->hInstance;
 
-    if (lppd->Flags & (PD_ENABLEPRINTTEMPLATE | PD_ENABLEPRINTTEMPLATEHANDLE |
-                       PD_ENABLESETUPTEMPLATE | PD_ENABLESETUPTEMPLATEHANDLE))
-        FIXME( "custom templates no longer supported, using default\n" );
+    if (lppd->Flags & PD_ENABLEPRINTTEMPLATE)
+    {
+        template_print = resource_to_dialog32(hInst, MapSL(lppd->lpPrintTemplateName));
+        pd32.hPrintTemplate = (HGLOBAL)template_print;
+        pd32.Flags |= PD_ENABLEPRINTTEMPLATEHANDLE;
+    }
+    if (lppd->Flags & PD_ENABLESETUPTEMPLATE)
+    {
+        template_setup = resource_to_dialog32(hInst, MapSL(lppd->lpSetupTemplateName));
+        pd32.hSetupTemplate = (HGLOBAL)template_setup;
+        pd32.Flags |= PD_ENABLESETUPTEMPLATEHANDLE;
+    }
+    if (lppd->Flags & (PD_ENABLEPRINTTEMPLATEHANDLE |
+                       PD_ENABLESETUPTEMPLATEHANDLE))
+        FIXME( "custom templates handle no longer supported, using default\n" );
     if (lppd->Flags & PD_ENABLEPRINTHOOK)
     {
         COMMDLGTHUNK *thunk = allocate_thunk(pd, lppd->lpfnPrintHook);
@@ -158,6 +187,8 @@ BOOL16 WINAPI PrintDlg16( SEGPTR pd )
     GlobalFree( pd32.hDevMode );
     delete_thunk(pd32.lpfnPrintHook);
     delete_thunk(pd32.lpfnSetupHook);
+    HeapFree(GetProcessHeap(), 0, template_setup);
+    HeapFree(GetProcessHeap(), 0, template_print);
     return ret;
 }
 
