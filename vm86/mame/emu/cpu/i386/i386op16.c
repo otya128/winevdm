aa689f461ecf27193ce74896c5e9ad3b9ed60909
otya <otya128@users.noreply.github.com>
2019-04-25 23:51:16 +0900

    Fix i386_device::i386_enter16

--- vm86/mame/emu/cpu/i386/i386op16.c
+++ vm86/mame/emu/cpu/i386/i386op16.c
@@ -1190,13 +1190,16 @@ static void I386OP(enter16)()           // Opcode 0xc8
 	else
 		frameptr = REG32(ESP);
 
-	if(level > 0)
+	if(level > 1)
 	{
 		for(x=1;x<level-1;x++)
 		{
 			REG16(BP) -= 2;
 			PUSH16(READ16(REG16(BP)));
 		}
+	}
+	else if (level == 1)
+	{
 		PUSH16(frameptr);
 	}
 	REG16(BP) = frameptr;
