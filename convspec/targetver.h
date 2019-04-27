9fb815a43214ba0c7bf951fa017d8f83973e5096
otya <otya128@users.noreply.github.com>
2019-04-27 18:59:21 +0900

    Remove unused files

--- convspec/targetver.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// SDKDDKVer.h をインクルードすると、利用できる最も上位の Windows プラットフォームが定義されます。
-
-// 以前の Windows プラットフォーム用にアプリケーションをビルドする場合は、WinSDKVer.h をインクルードし、
-// SDKDDKVer.h をインクルードする前に、サポート対象とするプラットフォームを示すように _WIN32_WINNT マクロを設定します。
-
-#include <SDKDDKVer.h>
