2f9e768c400ec314a754e40407b6a13f2f48993c
cracyc <cracyc@users.noreply.github.com>
2019-05-22 21:51:30 -0500

    more

--- msvideo/msvideo16.c
+++ msvideo/msvideo16.c
@@ -1009,6 +1009,9 @@ static LRESULT WINAPI MCIWndProc16(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lp
         lparam = (ULONG_PTR)MapSL(lparam);
         break;
 
+    case MCI_OPEN:
+        return CallWindowProcA(pMCIWndProc, hwnd, MCIWNDM_OPENA, 0, (ULONG_PTR)MapSL(lparam));
+
     case MCIWNDM_SETPALETTE:
         wparam = HPALETTE_32(wparam);
         break;
