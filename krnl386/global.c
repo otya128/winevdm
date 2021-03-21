460617321c558743534cfc6859204c6a7f0bc020
cracyc <cracyc@users.noreply.github.com>
2021-03-21 13:15:30 -0500

    it's possible the selector could change if there aren't enough to add to the end

--- krnl386/global.c
+++ krnl386/global.c
@@ -546,7 +546,7 @@ HGLOBAL16 WINAPI GlobalReAlloc16(
 
     if ((oldsize < size) && (flags & GMEM_ZEROINIT))
         memset( (char *)ptr + oldsize, 0, size - oldsize );
-    return handle;
+    return pNewArena->handle | (handle & 1);
 }
 
 
