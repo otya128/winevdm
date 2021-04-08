2bb86fcddadbb97a7ee28d980ef073a65bdf65b0
cracyc <cracyc@users.noreply.github.com>
2021-04-08 16:50:29 -0500

    if resource refcount is 0 load a new copy of the resource in loadresource

--- krnl386/resource.c
+++ krnl386/resource.c
@@ -1036,7 +1036,7 @@ HGLOBAL16 WINAPI LoadResource16( HMODULE16 hModule, HRSRC16 hRsrc )
 
     if (pNameInfo)
     {
-        if (pNameInfo->handle && GlobalHandle16(pNameInfo->handle) && !(GlobalFlags16(pNameInfo->handle) & GMEM_DISCARDED))
+        if (pNameInfo->handle && GlobalHandle16(pNameInfo->handle) && !(GlobalFlags16(pNameInfo->handle) & GMEM_DISCARDED) && pNameInfo->usage)
         {
             pNameInfo->usage++;
             TRACE("  Already loaded, new count=%d\n", pNameInfo->usage );
