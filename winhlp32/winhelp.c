otya128/winevdm

commit 29d71f79ef06798b37a96c2c2dbc43957202bc26

winhlp32: Add a workaround to fix use-after-free.
free WINHELP_WINDOW -> WINHELP_CheckPopup -> WINHELP_ReleaseWindow ->
--win->ref_count -> heap corruption

winhlp32/winhelp.c
@@ -677,7 +677,7 @@ BOOL WINHELP_ReleaseWindow(WINHELP_WINDOW* win)
 
     if (!--win->ref_count)
     {
-        DestroyWindow(win->hMainWnd);
+        PostMessageW(win->hMainWnd, WM_CLOSE, 0, 0);
         return FALSE;
     }
     return TRUE;

commit ec63432954ece62ae8254435a93d14352af50f0d

winhlp32: Fix MACRO_Exit infinite loop.
regression after 29d71f79ef06798b37a96c2c2dbc43957202bc26

winhlp32/winhelp.c
@@ -677,6 +677,26 @@ BOOL WINHELP_ReleaseWindow(WINHELP_WINDOW* win)
 
     if (!--win->ref_count)
     {
+        WINHELP_WINDOW**    w;
+
+        for (w = &Globals.win_list; *w; w = &(*w)->next)
+        {
+            if (*w == win)
+            {
+                *w = win->next;
+                break;
+            }
+        }
+
+        if (Globals.active_win == win)
+        {
+            Globals.active_win = Globals.win_list;
+            if (Globals.win_list)
+                SetActiveWindow(Globals.win_list->hMainWnd);
+        }
+
+        if (win == Globals.active_popup)
+            Globals.active_popup = NULL;
         PostMessageW(win->hMainWnd, WM_CLOSE, 0, 0);
         return FALSE;
     }