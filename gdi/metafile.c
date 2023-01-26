otya128/winevdm

commit 67e9228c90d89a7528b1559201e802ceb8ec99ea

if dlgproc is null don't pass wm_initdialog to the wndproc (#1239)

user/message.c
@@ -5223,7 +5223,8 @@ void register_wow_handlers(void)
 }
 
 BOOL is_dialog(HWND hwnd);
-HMENU get_dialog_hmenu(HWND hWnd);
+dialog_data *get_dialog_data(HWND hWnd);
+void free_proc_thunk(void *thunk);
 LRESULT CALLBACK WindowProc16(HWND hDlg, UINT Msg, WPARAM wParam, LPARAM lParam)
 {
     HWND16 hWnd16 = HWND_16(hDlg);
@@ -5234,9 +5235,19 @@ LRESULT CALLBACK WindowProc16(HWND hDlg, UINT Msg, WPARAM wParam, LPARAM lParam)
     /* some programs don't call DlgProc */
     if (Msg == WM_INITDIALOG && is_dialog(hDlg))
     {
-        if (!GetMenu(hDlg))
+	    dialog_data *dd = get_dialog_data(hDlg);
+        if (dd)
         {
-            SetMenu(hDlg, get_dialog_hmenu(hDlg));
+            if (!GetMenu(hDlg))
+                SetMenu(hDlg, HMENU_32(dd->hMenu16));
+            if (!dd->dlgProc)
+            {
+                void *thunk = (void *)GetWindowLongPtrA(hDlg, DWLP_DLGPROC);
+                SetWindowLongPtrA(hDlg, DWLP_DLGPROC, NULL);
+                HeapFree(GetProcessHeap(), 0, dd);
+                free_proc_thunk(thunk);
+                return TRUE;
+            }
         }
     }
     WNDPROC16 wndproc16 = (WNDPROC16)GetWndProc16(hWnd16);

user/dialog.c
@@ -52,12 +52,6 @@ typedef struct
 void free_proc_thunk(DLGPROCTHUNK *thunk);
 BYTE get_aflags(HMODULE16 hModule);
 
-typedef struct
-{
-    HMENU16 hMenu16;
-    DLGPROC16 dlgProc;
-} dialog_data;
-
 /* Dialog control information */
 typedef struct
 {
@@ -677,13 +671,12 @@ static void init_proc_thunk()
     MAX_THUNK = 4096 / sizeof(DLGPROCTHUNK);
     thunk_array = VirtualAlloc(NULL, MAX_THUNK * sizeof(DLGPROCTHUNK), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 }
-HMENU get_dialog_hmenu(HWND hWnd)
+dialog_data *get_dialog_data(HWND hWnd)
 {
     DLGPROC dlgproc = GetWindowLongPtrA(hWnd, DWLP_DLGPROC);
     if (thunk_array <= dlgproc && thunk_array + MAX_THUNK > dlgproc)
     {
-        DLGPROCTHUNK *thunk = (DLGPROCTHUNK*)dlgproc;
-        return HMENU_32(((dialog_data*)thunk->param)->hMenu16);
+        return (dialog_data*)(((DLGPROCTHUNK *)dlgproc)->param);
     }
     return 0;
 }

user/user_private.h
@@ -114,6 +114,12 @@ typedef struct tagDIALOGINFO
     UINT      flags;       /* EndDialog() called for this dialog */
 } DIALOGINFO;
 
+typedef struct
+{
+    HMENU16 hMenu16;
+    DLGPROC16 dlgProc;
+} dialog_data;
+
 #define DF_END  0x0001
 #define DF_OWNERENABLED 0x0002
 

commit 25a6b84ec1f795c3c210925bdb6d527d3d2cc124

user.exe16: Avoid double free of dialog_data.
1. WindowProc16(Msg=WM_INITDIALOG): SetMenu(hDlg, HMENU_32(dd->hMenu16));
2. WindowProc16(Msg=WM_WINDOWPOSCHANGING): CallWindowProcA(wndproc32, hDlg, Msg, wParam, lParam);
3. DlgProc_Thunk: SetWindowLongPtrA(hDlg, DWLP_DLGPROC, DlgProc);
4. DlgProc_Thunk: HeapFree(GetProcessHeap(), 0, cs);
5. DlgProc_Thunk: free_proc_thunk(thunk_data);
6. WindowProc16(Msg=WM_INITDIALOG): HeapFree(GetProcessHeap(), 0, dd); <= double free
7. WindowProc16(Msg=WM_INITDIALOG): free_proc_thunk(thunk = DlgProc); <= not heap

Fixes: 67e9228c90d8

gdi/metafile.c
@@ -103,13 +103,12 @@ HDC16 WINAPI CreateMetaFile16( LPCSTR filename )
     return HDC_16( CreateMetaFileA( filename ) );
 }
 
-void delete_dc_entry(HDC hdc32);
 /******************************************************************
  *	     CloseMetaFile     (GDI.126)
  */
 HMETAFILE16 WINAPI CloseMetaFile16(HDC16 hdc)
 {
-    return create_metafile16(CloseMetaFile(HDC_32(hdc)));
+    return create_metafile16( CloseMetaFile( HDC_32(hdc) ));
 }
 
 /******************************************************************

user/message.c
@@ -5246,12 +5246,13 @@ LRESULT CALLBACK WindowProc16(HWND hDlg, UINT Msg, WPARAM wParam, LPARAM lParam)
     /* some programs don't call DlgProc */
     if (Msg == WM_INITDIALOG && is_dialog(hDlg))
     {
-	    dialog_data *dd = get_dialog_data(hDlg);
+        dialog_data *dd = get_dialog_data(hDlg);
         if (dd)
         {
-            if (!GetMenu(hDlg))
+            if (!GetMenu(hDlg) && dd->hMenu16)
                 SetMenu(hDlg, HMENU_32(dd->hMenu16));
-            if (!dd->dlgProc)
+            dd = get_dialog_data(hDlg);
+            if (dd && !dd->dlgProc)
             {
                 void *thunk = (void *)GetWindowLongPtrA(hDlg, DWLP_DLGPROC);
                 SetWindowLongPtrA(hDlg, DWLP_DLGPROC, NULL);