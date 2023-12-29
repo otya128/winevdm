otya128/winevdm

commit 6e22d1243d30809eae4d4eda418a9ed547e42bcd

Fix FINDFILE_DTA to capitalize

krnl386/int21.c
@@ -3955,11 +3955,15 @@ static BOOL INT21_FindNext( CONTEXT *context )
         dta->fileattr = entry.dwFileAttributes;
         dta->filesize = entry.nFileSizeLow;
         FileTimeToDosDateTime( &entry.ftLastWriteTime, &dta->filedate, &dta->filetime );
+        memset(dta->filename, 0, 13);
         if (entry.cAlternateFileName[0])
             WideCharToMultiByte(CP_OEMCP, 0, entry.cAlternateFileName, -1,
                                 dta->filename, 13, NULL, NULL);
         else
             WideCharToMultiByte(CP_OEMCP, 0, entry.cFileName, -1, dta->filename, 13, NULL, NULL);
+        /* shoud be UPPER CASE */
+        for (int i = 0; i < 13; i++)
+            dta->filename[i] = toupper(dta->filename[i]);
 
         if (!memchr(dta->mask,'?',11))
         {

commit cf4c32bca3e1bd2d3e12198532e604b4249e5ce5

krnl386.exe16: Fix  handling of DBCS characters in FINDNEXT.
Fixes: 6e22d1243d30809eae4d4eda418a9ed547e42bcd

krnl386/int21.c
@@ -4067,13 +4067,20 @@ static BOOL INT21_FindNext( CONTEXT *context )
         FileTimeToDosDateTime( entry.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ? &entry.ftCreationTime : &entry.ftLastWriteTime, &dta->filedate, &dta->filetime );
         memset(dta->filename, 0, 13);
         if (entry.cAlternateFileName[0])
+        {
+            /* shoud be UPPER CASE */
+            for (int i = 0; i < 13; i++)
+                entry.cAlternateFileName[i] = toupperW(entry.cAlternateFileName[i]);
             WideCharToMultiByte(CP_OEMCP, 0, entry.cAlternateFileName, -1,
                                 dta->filename, 13, NULL, NULL);
+        }
         else
+        {
+            /* shoud be UPPER CASE */
+            for (int i = 0; i < 13; i++)
+                entry.cFileName[i] = toupperW(entry.cFileName[i]);
             WideCharToMultiByte(CP_OEMCP, 0, entry.cFileName, -1, dta->filename, 13, NULL, NULL);
-        /* shoud be UPPER CASE */
-        for (int i = 0; i < 13; i++)
-            dta->filename[i] = toupper(dta->filename[i]);
+        }
 
         if (!memchr(dta->mask,'?',11))
         {