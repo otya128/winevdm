c202965755e4a029f5896e15597ada436550aac5
cracyc <cracyc@users.noreply.github.com>
2024-10-11 16:49:39 -0500

    set the current directory to lpdir in shellexecute then set it back on return

--- shell/shell.c
+++ shell/shell.c
@@ -706,7 +706,11 @@ static UINT_PTR SHELL_Execute16(const WCHAR *lpCmd, WCHAR *env, BOOL shWait,
 static UINT_PTR WINAPI SHELL_Execute16_Windows(const CHAR *lpCmd, int nShowCmd, const CHAR *lpDir)
 {
 	UINT ret;
+	char currdir[MAX_PATH];
+	GetCurrentDirectoryA(MAX_PATH, currdir);
+	SetCurrentDirectoryA(lpDir);
 	ret = WinExec16(lpCmd, nShowCmd);
+	SetCurrentDirectoryA(currdir);
 	return ret;
 }
 
